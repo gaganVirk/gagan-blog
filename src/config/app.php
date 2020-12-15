5987a5805e2f0514985c94dca13a20a1b7eb8951
Gagan Virk <gagan@ocular.co.nz>
2020-12-15 16:07:38 +1300

    included logo script in config

--- src/config/app.php
+++ src/config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'logo' => env('APP_LOGO'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
